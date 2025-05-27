a8475e232ac2c806c36956ad2d69f42c0a3cc39d
Joy Guevarra <joguevarra5@up.edu.ph>
2025-05-28 02:20:01 +0800

    feat: added initial main page

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,22 @@
 import express from "express";
-import { getOrgs } from './index.js';
+import orgService from './index.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 // endpoints
 
 app.get("/orgs", async (req, res) => {
-    const result = await getOrgs();
+    const result = await orgService.getOrgs();
     res.send(result);
 });
 
+app.get("/org-names", async (req, res) => {
+    const result = await orgService.getOrgs();
+    const namesOnly = result.map(org => ({ org_name: org.org_name }));
+    res.send(namesOnly);
+});
 
 
 
--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,64 @@ const pool = mysql.createPool({
     database: 'orgdb',
 }).promise();
 
-// org table 
+// org functions
 
 export async function getOrgs() {
     const [result] = await pool.query("SELECT * FROM org");
     return result;
 }
 
-// org_mem table
+export async function addOrg(org_name, classification) {
+    await pool.query(
+        `INSERT INTO org (org_name, classification)
+        VALUES (?, ?)`
+        , [org_name, classification]
+    );
+}
 
-// student
+export async function updateOrg(newName, newClassification, org_id) {
+    await pool.query(
+        `UPDATE org
+        SET org_name = ?, classification = ?
+        WHERE org_id = ?`
+        , [newName, newClassification, org_id]
+    );
+}
 
-// fee
+export async function deleteOrg(org_id) {
+    await pool.query(
+        `DELETE org
+        WHERE org_id = ?`
+        , [org_id]
+    );
+}
 
-const result = await getOrgs();
+// member functions
+
+export async function getMembersByOrg() {
+    const [result] = await pool.query(
+        `SELECT o.org_name, om.position, om.status, s.gender, s.degprog, om.join_date, om.committee
+        FROM student AS s JOIN org_mem AS om ON s.student_id = om.student_id
+        JOIN org AS o ON om.org_id = o.org_id;`
+    )
+    return result;
+}
+
+// TODO: addMember, editMember, deleteMember
+
+// fee functions
+
+export async function getPendingFees() {
+
+}
+
+const result = await getMembersByOrg();
 console.log(result);
+
+export default {
+    getOrgs,
+    addOrg,
+    updateOrg,
+    deleteOrg,
+    getMembersByOrg,
+};
