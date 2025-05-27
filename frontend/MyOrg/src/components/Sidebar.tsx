b1aeeb758499aed2b464881b20011dd50cb69db3
Joy Guevarra <joguevarra5@up.edu.ph>
2025-05-28 04:15:29 +0800

    feat: add to org functionality

--- frontend/MyOrg/src/components/Sidebar.tsx
+++ frontend/MyOrg/src/components/Sidebar.tsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../assets/images/logo.png';
-import AddOrgModal from './AddOrgModal'
+import AddOrgModal from './AddOrgModal'; // adjust path as needed
 
 function Sidebar() {
     const [orgs, setOrgs] = useState([]);
     const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
     useEffect(() => {
         fetchOrgs();
     }, []);
 
     const fetchOrgs = () => {
-        fetch("http://localhost:8080/orgs")
+        fetch("http://localhost:8080/org-names")
             .then(res => res.json())
             .then(data => setOrgs(data))
             .catch(err => console.error("Failed to fetch orgs:", err));
@@ -21,31 +20,10 @@ function Sidebar() {
     const openAddModal = () => setIsAddModalOpen(true);
     const closeAddModal = () => setIsAddModalOpen(false);
 
-    const openEditModal = () => setIsAddModalOpen(true);
-    const closeEditModal = () => setIsAddModalOpen(false);
-
     const handleAddOrg = async (orgName, classification) => {
         try {
             const res = await fetch('http://localhost:8080/org-add', {
                 method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ org_name: orgName, classification }),
-            });
-
-            if (!res.ok) throw new Error('Failed to add org');
-
-            closeAddModal();
-            fetchOrgs();
-        } catch (err) {
-            console.error(err);
-            alert('Error adding org');
-        }
-    };
-
-    const handleEditOrg = async (orgName, classification) => {
-        try {
-            const res = await fetch('http://localhost:8080/org-edit', {
-                method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ org_name: orgName, classification }),
             });
@@ -76,18 +54,8 @@ function Sidebar() {
 
             <div>
                 {orgs.map((org) => (
-                    <div className="flex items-center justify-end">
-                        <div className="flex justify-center pr-4" id="orgTile">
-                            <button className="mr-4">
-                                <i className="fa fa-trash" style={{ color: 'white' }} aria-hidden="true"></i>
-                            </button>
-
-                            <button>
-                                <i className="fa fa-pencil" style={{ color: 'white' }} aria-hidden="true"></i>
-                            </button>
-                        </div>
-
-                        <button key={org.org_name} className="items-start w-full text-white m-2 text-left">
+                    <div>
+                        <button key={org.org_name} className="items-start text-2xl w-full text-white m-2 text-left">
                             {org.org_name}
                         </button>
                     </div>
@@ -96,10 +64,6 @@ function Sidebar() {
 
             {isAddModalOpen && (
                 <AddOrgModal onCancel={closeAddModal} onSave={handleAddOrg} />
-            )}
-
-            {isEditModalOpen && (
-                <EditOrgModal onCancel={closeAddModal} onSave={handleAddOrg} />
             )}
         </div>
     );
